9e5ca02b91294a6c0b0b346952f0eab154ac70b7
caitlyn-homa <cmh299@pitt.edu>
2025-03-25 21:53:40 -0400

    Update/organize tune_it_main.ino

--- tune_it_main.ino
+++ tune_it_main.ino
@@ -3,195 +3,247 @@
 #include <SPI.h>
 #include <Wire.h>
 
-//#include "SparkFunLIS3DH.h"
-
-
-//RESET Button variables
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin = 0;  // the number of the pushbutton pin
-const int ledPin = 8;    // the number of the LED pin
-const byte interruptPin = 3;
-
-//Other variables
-int buttonState = 0;  // variable for reading the pushbutton status
-int score = 0;  // keep track of score 
-int iteration = 0; // keep track of game loop iteration 
-bool actionCompleted = false // holds if player has completed task of not 
-
-//Countdown variables 
-unsigned long previous_time = 0 //holds previous time and how many times countdown has been updated
-unsigned long current_time = 0 //using millis() to get current time to begin a timer
-const long conversion = 1000 //1000 miliseconds in a second
-int interval = 3; // holds time to complete action (in seconds)
-int countdown = interval; // will be countdown variable when action is called 
-
-void startGame();
-void lose();
-void win();
-void reset_pressed();
-bool pull(int time_lim);
-
-//OLED DISPLAY set-up 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-//LIS3DH SensorOne( I2C_MODE, 0x19 );
+//Keep track of player's score throughout the game
+int score = 0;
+//Initial time player has to complete a task [seconds]
+int time_to_complete = 3;
 
 void setup() {
-  //Initialize communication with computer from microprocessor 
-  Serial.begin(9600);
-  //Seed random number generation 
-  randomSeed(analogRead(0));
+  //Seed random number generation with current time -ensures variation in the sequence of random numbers 
+  randomSeed(millis());
 
-  //PULL TASK set-up 
-   // initialize the LED pin as an output:
+  //Pin Set-Up 
+  // Reset/Start button
   pinMode(8, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(2, INPUT_PULLUP);
-
-  //TUNER TASK set-up - declare the ledPin as an OUTPUT 
+  pinMode(3, INPUT_PULLUP);
+  // Pull Task
   pinMode(8, OUTPUT);
-
-  //SHAKE TASK set-up 
+  pinMode(1, INPUT_PULLUP);
+  // Tune Task
   pinMode(8, OUTPUT);
+  // Shake Task
+  pinMode(8, OUTPUT);
+  // Stepper Motor 
+  pinMode(5, OUTPUT); //st_a
+  pinMode(6, OUTPUT); //st_b
+  pinMode(7, OUTPUT); //st_c
+  pinMode(8, OUTPUT); //st_d
+  //pinMode(0, INPUT_PULLUP);
+  // OLED Display
+  pinMode(8, OUTPUT);  
+  // Audio player 
 
-  //STEPPER MOTOR set-up
+  //OLED set-up - uncomment the function - i don't have library installed for it :(
+  //oled_set_up();
+
+}
+
+void loop() {
+  
+  //If start button has been pushed - begin the game 
+  if (digitalRead(3) == LOW){
+    
+    //Generates random number 1 through 3 to select action - MAP -> #1 = Pull-it #2 = Tune-it #3 = Shake-it
+    byte action_number = random(1,4);
+
+  }
+
+}
+
+//Returns true or false depending if player has succesfully shaken the radio
+//Parameter - time_lime = time player has to complete the task 
+bool has_shaken(int time_lim)
+{
+  //Convert time limit to milliseconds
+  time_lim = time_lim * 1000
+
+  //Magnitude of acceleration that player must meet to succesfully complete shake it task
+  float threshold = 1.25;
+
+  //Begin countdown 
+  int starttime = millis();
+  int endtime = starttime;
+  while ((endtime - starttime) <= time_lim) //Runs until time limit or response
+  {
+    //Read sensor data for accleration in x, y, and z directions 
+    float a_x = SensorOne.readFloatAccelX();
+    float a_y = SensorOne.readFloatAccelY();
+    float a_z = SensorOne.readFloatAccelZ();
+
+    //Calculate the accleration magnitude detected
+    float mag = sqrt(a_x*a_x + a_y*a_y + a_z*a_z);
+
+    //If accleration is greater than threshold - player has passed
+    if(mag > thresh)
+    {
+      return true 
+    }
+    
+    //Update time passed
+    endtime = millis();
+    }
+
+  //Player failed 
+  return false;
+}
+
+//Returns true or false depending if player has succesfully pulled the antenna 
+//Parameter - time_lime = time player has to complete the task 
+bool has_pulled(int time_lim)
+{
+  //Convert time limit to milliseconds
+  time_lim = time_lim * 1000
+
+  //Set up countdown variables and begin the timer
+  int starttime = millis();
+  int endtime = starttime;
+
+  //Get button's inital state 
+  int initState = digitalRead(2);
+  while ((endtime - starttime) <= time_lim) //Runs until time limit or response
+  {
+    //Check button's state - Has player succesfully pulled the antenna
+    int buttonState = digitalRead(2);
+
+    //If transistion in button's state is detected -> player has succeeded
+    if((buttonState - initState) != 0) 
+      {
+        return true;
+      }
+  
+  //Update time passed
+  endtime = millis();
+  }
+
+  //Player failed 
+  return false;
+}
+
+//Returns true or false depending on if player has succesfully tuned the knob 
+//Parameter - time_lime = time player has to complete the task 
+bool has_tuned(int time_lim)
+{
+  //Convert time limit to milliseconds
+  time_lim = time_lim * 1000
+
+  //Set a threshold that the knob must be turned to for a successful attempt 
+  int thresh = 50;
+
+  //Obtain current time and begin the countdown  
+  int starttime = millis();
+  int endtime = starttime;
+
+  //Read the starting voltage at the tuning knob 
+  int start_voltage = analogRead(A0);
+
+  while ((endtime - starttime) <= time_lim) // do this loop for up to 1000mS
+  {
+    //Read current voltage at the tuning knob 
+    int current_voltage = analogRead(A0);
+    //If the difference is greater than the threshold - task is completed 
+    if(abs(current_voltage - start_voltage) > thresh)
+      {
+        return true;
+      }
+
+  //Update time passed
+  endtime = millis();
+  }
+
+  //Player failed
+  return false;
+}
+
+//Rotates motor needs specific time step and angle 
+void rotate_motor(int t_step, int angle)
+{
+  //Pins where motor is set up at 
   int st_a = 5;
   int st_b = 6;
   int st_c = 7;
   int st_d = 8;
 
-  pinMode(st_a, OUTPUT);
-  pinMode(st_b, OUTPUT);
-  pinMode(st_c, OUTPUT);
-  pinMode(st_d, OUTPUT);
-  pinMode(0, INPUT_PULLUP);
-
-  //RESET BUTTON set-up
-  // initialize the LED pin as an output:
-  pinMode(8, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(0, INPUT_PULLUP);
-
-  //digital pin 3 to trigger an interrupt to reset the game - will run reset_pressed protocol when it detects a change at digital pin 3
-  attachInterrupt(digitalPinToInterrupt(interruptPin), reset_pressed, CHANGE);
-
-
-}
-
-//MAIN LOOP
-void loop() 
-{
-  //If start button has been pushed (pin #)
-    startGame()
-
-    //Loop over 99 times 
-    for(iteration = 0; iteration<99; iteration++) {
-      //Should generate a random number between 1 and 3 MAPPING -> #1 = Pull the Antenna #2 = Tune the Radio #3 = Shake the Radio
-      action = random(1,4);
-
-      //Run through Pull the Antenna action sequences
-      if(action == 1)
-      {
-        //Cut out radio song 
-        //Play Pull it! sound 
-        //Begin countdown by obtaining the current time
-        current_time = millis()
-
-        //If a second hasn't passed 
-        if(current_time - previous_time >= conversion)
-        {
-          previous_time = current_time 
-
-          //And if there is still time left and action is not yet completed --> 
-          if(countdown >= 0 && !actionCompleted)
-          {
-            countdown--;
-          }
-          //Else countdown hit zero and player did not complete task
-            else
-            {
-              //Player has lost
-              lose()
-            }
-        }
-    }
-
-    //Decrease interval every 5th iteration or so - for 99 iterations - will decrease 19 times 
-    if(iteration % 5 == 0){
-      //Decrease countdown interval -- decrease a total of 2.85 seconds if player makes it to the end 
-      interval = interval - 0.15
-      //Reset countdown back to interval 
-      countdown = interval
-    }
-    else {
-      //Reset countdown back to interval
-      countdown = interval
-    }
-  }
-
-
-}
-
-//Function to reset back to start of game conditions
-void startGame() {
-
-  int score = 0;  // keep track of score 
-  int iteration = 0; // keep track of game loop iteration 
-  int interval = 0; // holds time to complete action
-  int countdown = 0; // will countdown when action is called 
-
-  //Reset motor too 
-
-  //Play indication game will begin 
-
-  return;
-}
-
-//Function to carry out steps when player has lost 
-void lose() {
+  //Calculate number of steps needed to rotate for specified angle
+  int steps = int(angle*(8.0/5.625));
   
-  iteration = 100; //Set iteration to 100 to not confuse with a win but exits game loop 
-  //indicate loss 
-  //display final score 
-
-  return;
-}
-
-//Function to carry out the steps when player has won 
-void win(){
-  //play winning sound - sweet caroline? 
-
-  return;
-}
-
-void reset_pressed(){
-
-  //indicate reset has been pressed somehow
-  iteration = 101;
-
-  return;
-
-}
-
-bool pull(int time_lim)
-{
-  int starttime = millis();
-  int endtime = starttime;
-  int initState = digitalRead(2);
-  while ((endtime - starttime) <= time_lim) //Runs until time limit or response
+  //Drive the stepper motor using appropriate pins for each step 
+  for(int i = 0; i < steps; ++i)
   {
-    int buttonState = digitalRead(2);
-    if((buttonState - initState) != 0) //Detects transision
-      {
-        return true;
-      }
-
-  endtime = millis();
+    digitalWrite(st_a, LOW); //A
+    digitalWrite(st_b, HIGH);
+    digitalWrite(st_c, HIGH);
+    digitalWrite(st_d, HIGH);
+    delay(t_step);
+    digitalWrite(st_a, LOW); //AB
+    digitalWrite(st_b, LOW);
+    digitalWrite(st_c, HIGH);
+    digitalWrite(st_d, HIGH);
+    delay(t_step);
+    digitalWrite(st_a, HIGH); //B
+    digitalWrite(st_b, LOW);
+    digitalWrite(st_c, HIGH);
+    digitalWrite(st_d, HIGH);
+    delay(t_step);
+    digitalWrite(st_a, HIGH); //BC
+    digitalWrite(st_b, LOW);
+    digitalWrite(st_c, LOW);
+    digitalWrite(st_d, HIGH);
+    delay(t_step);
+    digitalWrite(st_a, HIGH); //C
+    digitalWrite(st_b, HIGH);
+    digitalWrite(st_c, LOW);
+    digitalWrite(st_d, HIGH);
+    delay(t_step);
+    digitalWrite(st_a, HIGH); //CD
+    digitalWrite(st_b, HIGH);
+    digitalWrite(st_c, LOW);
+    digitalWrite(st_d, LOW);
+    delay(t_step);
+    digitalWrite(st_a, HIGH); //D
+    digitalWrite(st_b, HIGH);
+    digitalWrite(st_c, HIGH);
+    digitalWrite(st_d, LOW);
+    delay(t_step);
+    digitalWrite(st_a, LOW); //AD
+    digitalWrite(st_b, HIGH);
+    digitalWrite(st_c, HIGH);
+    digitalWrite(st_d, LOW);
+    delay(t_step);
   }
-  return false;
 }
 
+/*
+//Sets up the OLED and displays Tune It! message 
+void oled_set_up(){
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Tune It!");
+  display.display(); 
+
+  Serial.begin(9600);
+  delay(1000); //relax...
+  Serial.println("Processor came out of reset.\n");
+  
+  //Call .begin() to configure the IMUs
+  if( SensorOne.begin() != 0 )
+  {
+	  Serial.println("Problem starting the sensor at 0x19.");
+  }
+  else
+  {
+	  Serial.println("Sensor at 0x19 started.");
+  }
+}
+*/
 
