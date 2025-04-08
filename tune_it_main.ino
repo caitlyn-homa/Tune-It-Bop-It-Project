1cf642eadd90bb84a8de070afba6e60288c2c365
SawyerMervis <156098417+SawyerMervis@users.noreply.github.com>
2025-04-08 16:00:47 -0400

    Update tune_it_main.ino

--- tune_it_main.ino
+++ tune_it_main.ino
@@ -1,425 +1,206 @@
+// Optimized Bop-It Game Code for ATMega328 (Memory-efficient)
 #include <DFMiniMp3.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <SPI.h>
 #include <Wire.h>
 #include "SparkFunLIS3DH.h" 
-
 #include "Mp3Manager.hpp"
 
-//Keep track of player's score throughout the game
-byte score = 0;
-//Initial time player has to complete a task [seconds]
-float time_to_complete = 4.0;
-//Keeps track whether action was successfully completed or not 
-bool isComplete = false;
-//Keeps track of what round player is on - will be used to exit loop if player loses 
-byte iteration = 0;
-//Flag for if game is restarting 
-bool restartPressed = false;
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-LIS3DH SensorOne( I2C_MODE, 0x19 );
 
-Mp3Manager mp3; // instance your class per your design
+LIS3DH SensorOne(I2C_MODE, 0x19);
+Mp3Manager mp3;
+
+// Game state
+byte score = 0;
+float time_to_complete = 4.0;
+int volume = analogRead(A3)/20.5;
+bool isComplete = false;
+byte iteration = 0;
+volatile bool restartPressed = false;
+
+// Memory check (debugging only)
+extern int __heap_start, *__brkval;
+int freeMemory() {
+  int v;
+  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+}
 
 void setup() {
-  //Seed random number generation with current time -ensures variation in the sequence of random numbers 
-  randomSeed(analogRead(A1));
+  pinMode(3, INPUT_PULLUP); // Start button
+  attachInterrupt(digitalPinToInterrupt(3), restart_game, FALLING);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP); // Pull sensor
+  pinMode(A0, INPUT);        // Potentiometer
 
-  //Pin Set-Up 
-  // Reset/Start button
-  pinMode(8, OUTPUT);
-  pinMode(3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(3), restart_game, LOW);
-  // Pull Task
-  pinMode(2, INPUT_PULLUP);
-  // Tune Task
-  pinMode(A0, INPUT);
-  // Shake Task
-  pinMode(A5, INPUT);
-  // Stepper Motor 
-  pinMode(5, OUTPUT); //st_a
-  pinMode(6, OUTPUT); //st_b
-  pinMode(7, OUTPUT); //st_c
-  pinMode(8, OUTPUT); //st_d
-  // OLED Display
-  pinMode(A4, OUTPUT);  
-  // Speaker 
-  pinMode(0, INPUT);
-  pinMode(1, OUTPUT);
+  // Stepper motor pins
+  for (int i = 5; i <= 8; i++) pinMode(i, OUTPUT);
 
-  // Volume 
-  pinMode(A3, INPUT);
-
-  //OLED set-up 
-  oled_set_up();
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.display();
 
   mp3.begin();
+  SensorOne.begin();
 }
 
 void loop() {
-
   waitMilliseconds(100);
-  oled_display_str("Press Start", 2);
+  oled_display_str(F("Press Start"), 2);
 
-  //If start of new game 
-  if(restartPressed)
-  {
-    //Reset start button flag 
+  if (restartPressed) {
     restartPressed = false;
-
-    //Put score motor back to far right
-    motor_reset();          
-  
-    //Indicate to player game is about to begin and have a countdown 
-    oled_display_str("Get Ready!", 2);
+    motor_reset();
+    oled_display_str(F("Get Ready!"), 2);
     oled_countdown(3);
 
-    //Start music 
-
-    //Game loop - Player wins after 99 rounds 
-    for(iteration = 0; iteration<99; iteration++)
-    {
-      //Reset action completion variable back to false
+    for (iteration = 0; iteration < 99; iteration++) {
+      mp3.loop();
       isComplete = false;
+      byte action_number = random(1, 4);
 
-      //Generates random number 1 through 3 to select action - MAP -> #1 = Pull-it #2 = Tune-it #3 = Shake-it
-      byte action_number = random(1,4);
-
-      //Pull-It! 
-      if(action_number == 1)
-      {
-        //Cut out sound/music
-        //Play Pull it! sound
-        oled_display_str("Pull It!", 2);
-        delay(200);
+      if (action_number == 1) {
+        oled_display_str(F("Pull It!"), 2);
+        waitMilliseconds(300);
         isComplete = has_pulled(time_to_complete);
-
-        //If action is completed within the time 
-        if (isComplete)
-        {
-          //Continue music 
-          score++; 
-          rotate_motor(1,2);
-          delay(500);
-        } 
-        else {
-          //Go to steps to take if they lost 
-          player_lost(score);
-        }
-      }
-      //Tune-It!
-      if(action_number == 2)
-      {
-        //Change music/cut out 
-        //Play Tune it! sound
-        oled_display_str("Tune It!", 2);
-        delay(500);
+      } else if (action_number == 2) {
+        oled_display_str(F("Tune It!"), 2);
+        waitMilliseconds(300);
         isComplete = has_tuned(time_to_complete);
-
-        //If action is completed within the time 
-        if (isComplete)
-        {
-          //Continue music 
-          score++; 
-          rotate_motor(1,2);
-          delay(500);
-        } 
-        else {
-          //Cue loss protocol
-          player_lost(score);
-        }
-      }
-      //Shake-It!
-      if(action_number == 3)
-      {
-        //Play static sound 
-        oled_display_str("Shake It!", 2);
-        delay(500);
+      } else {
+        oled_display_str(F("Shake It!"), 2);
+        waitMilliseconds(300);
         isComplete = has_shaken(time_to_complete);
-
-        //If action is completed within the time 
-        if (isComplete)
-        {
-          //Continue music 
-          score++; 
-          rotate_motor(1,2);
-          delay(500);
-        } 
-        else {
-          //Cue loss protocol
-          player_lost(score);
-        }
-      }
-      //End of actions 
-
-      //Decrease time to complete action every 5th iteration or so - for 99 iterations - will decrease 19 times
-      if(iteration % 5 == 0)
-      {
-        //Will decrease by 2.85 seconds if player makes it to the end 
-        time_to_complete = time_to_complete - 0.15;
       }
 
-    } //end of for loop
+      if (isComplete) {
+        score++;
+        rotate_motor(1, 2);
+        waitMilliseconds(300);
+      } else {
+        player_lost(score);
+        break;
+      }
 
-    //Player has won - indicate the win somehow
-    if(score == 99)
-    {
-      player_won();
+      if (iteration % 5 == 0) time_to_complete -= 0.15;
     }
-  }
 
-  //Reset iteration to 0 to start new game
-  iteration = 0;
+    if (score == 99) player_won();
+    iteration = 0;
+  }
 }
 
-//Sets restart flag to true to know when to begin a new game 
-void restart_game()
-{
-  score = 0;              //Make sure score is at 0 
-  time_to_complete = 4;   //Reset initial time to complete action 
-
-  //If currently in game 
-  if(iteration > 0)
-  {
-    //Exit game and set restart back to false - player will have to press the button again to start a new game
-    iteration = 101;
-    restartPressed = false;
-    oled_display_str("Restarting", 2);
-  }
-  //Else, new game is about to begin
-  else
-  {
+void restart_game() {
+  static unsigned long lastInterruptTime = 0;
+  unsigned long currentTime = millis();
+  if (currentTime - lastInterruptTime > 300) {
+    lastInterruptTime = currentTime;
+    score = 0;
+    time_to_complete = 4;
+    iteration = 0;
     restartPressed = true;
   }
-  
 }
 
-//IN-GAME FUNCTIONS 
+bool has_shaken(float time_lim) {
+  int duration = (int)(time_lim * 1000);
+  float threshold = 1.25;
+  unsigned long start = millis();
 
-//Steps to follow if player has lost 
-void player_lost(byte score)
-{
-  oled_display_str("You Lost", 3);
-  delay(2000); 
-  //Play broken radio sounds 
+  while ((millis() - start) <= duration) {
+    float ax = SensorOne.readFloatAccelX();
+    float ay = SensorOne.readFloatAccelY();
+    float az = SensorOne.readFloatAccelZ();
+    float mag = sqrt(ax * ax + ay * ay + az * az);
+    if (mag > threshold) return true;
+    mp3.loop();
+  }
+  return false;
+}
 
-  oled_display_str("Final Score:", 3);
-  delay(2000);
-  oled_display_score(score);
-  delay(2000); 
+bool has_pulled(float time_lim) {
+  int duration = (int)(time_lim * 1000);
+  int startState = digitalRead(2);
+  unsigned long start = millis();
 
-  //Set iteration to 101 to exit game loop
+  while ((millis() - start) <= duration) {
+    if (digitalRead(2) != startState) return true;
+    mp3.loop();
+  }
+  return false;
+}
+
+bool has_tuned(float time_lim) {
+  int duration = (int)(time_lim * 1000);
+  int threshold = 50;
+  int startVal = analogRead(A0);
+  unsigned long start = millis();
+
+  while ((millis() - start) <= duration) {
+    if (abs(analogRead(A0) - startVal) > threshold) return true;
+    mp3.loop();
+  }
+  return false;
+}
+
+void player_lost(byte final_score) {
+  oled_display_str(F("You Lost"), 2);
+  waitMilliseconds(1500);
+  oled_display_str(F("Score:"), 2);
+  oled_display_score(final_score);
+  waitMilliseconds(2000);
   iteration = 101;
 }
 
-//Steps to follow if player has won
-void player_won()
-{
-  oled_display_str("Congrats!", 3);
-  delay(2000);
-  oled_display_str("You Win!", 3);
-  delay(2000);
-
-  //Play sweet caroline 
-
+void player_won() {
+  oled_display_str(F("Congrats!"), 2);
+  waitMilliseconds(2000);
+  oled_display_str(F("You Win!"), 2);
+  waitMilliseconds(2000);
 }
 
-//Returns true or false depending if player has succesfully shaken the radio
-//Parameter - time_lime = time player has to complete the task 
-bool has_shaken(int time_lim)
-{
-  //Convert time limit to milliseconds
-  time_lim = time_lim * 1000;
-
-  //Magnitude of acceleration that player must meet to succesfully complete shake it task
-  float threshold = 1.25;
-
-  //Begin countdown 
-  int starttime = millis();
-  int endtime = starttime;
-  while ((endtime - starttime) <= time_lim) //Runs until time limit or response
-  {
-    //Read sensor data for accleration in x, y, and z directions 
-    float a_x = SensorOne.readFloatAccelX();
-    float a_y = SensorOne.readFloatAccelY();
-    float a_z = SensorOne.readFloatAccelZ();
-
-    //Calculate the accleration magnitude detected
-    float mag = sqrt(a_x*a_x + a_y*a_y + a_z*a_z);
-
-    //If accleration is greater than threshold - player has passed
-    if(mag > threshold)
-    {
-      return true;
-    }
-    
-    //Update time passed
-    endtime = millis();
-    }
-
-  //Player failed 
-  return false;
-}
-
-//Returns true or false depending if player has succesfully pulled the antenna 
-//Parameter - time_lime = time player has to complete the task 
-bool has_pulled(int time_lim)
-{
-  //Convert time limit to milliseconds
-  time_lim = time_lim * 1000;
-
-  //Set up countdown variables and begin the timer
-  int starttime = millis();
-  int endtime = starttime;
-
-  //Get button's inital state 
-  int initState = digitalRead(2);
-  while ((endtime - starttime) <= time_lim) //Runs until time limit or response
-  {
-    //Check button's state - Has player succesfully pulled the antenna
-    int buttonState = digitalRead(2);
-
-    //If transistion in button's state is detected -> player has succeeded
-    if((buttonState - initState) != 0) 
-      {
-        return true;
-      }
-  
-  //Update time passed
-  endtime = millis();
-  }
-
-  //Player failed 
-  return false;
-}
-
-//Returns true or false depending on if player has succesfully tuned the knob 
-//Parameter - time_lime = time player has to complete the task 
-bool has_tuned(int time_lim)
-{
-  //Convert time limit to milliseconds
-  time_lim = time_lim * 1000;
-
-  //Set a threshold that the knob must be turned to for a successful attempt 
-  byte thresh = 50;
-
-  //Obtain current time and begin the countdown  
-  int starttime = millis();
-  int endtime = starttime;
-
-  //Read the starting voltage at the tuning knob 
-  int start_voltage = analogRead(A0);
-
-  while ((endtime - starttime) <= time_lim) // do this loop for up to 1000mS
-  {
-    //Read current voltage at the tuning knob 
-    int current_voltage = analogRead(A0);
-    //If the difference is greater than the threshold - task is completed 
-    if(abs(current_voltage - start_voltage) > thresh)
-      {
-        return true;
-      }
-
-  //Update time passed
-  endtime = millis();
-  }
-
-  //Player failed
-  return false;
-}
-
-//OLED FUNCTIONS 
-
-//Sets up the OLED and displays Tune It! message 
-void oled_set_up(){
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-
-  delay(200);
+void oled_display_str(const __FlashStringHelper *disp, byte size) {
   display.clearDisplay();
-
-  display.setTextSize(4);
+  display.setTextSize(size);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  // Display static text
-  display.println("Tune It!");
-  display.display(); 
-
-  Serial.begin(9600);
-  delay(100); 
-  Serial.println("Processor came out of reset.\n");
-  
-  //Call .begin() to configure the IMUs
-  if( SensorOne.begin() != 0 )
-  {
-	  Serial.println("Problem starting the sensor at 0x19.");
-  }
-  else
-  {
-	  Serial.println("Sensor at 0x19 started.");
-  }
-}
-
-//Displays score on oled - always at size 4 
-void oled_display_score(byte score){
-
-  delay(10);
-  display.clearDisplay();
-  display.setTextSize(4);
-
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // Display static text
-  display.println(score);
-  display.display(); 
-
-}
-
-//Display text to oled at specified text size 
-void oled_display_str(char* disp, byte text_size){
-
-  delay(10);
-  display.clearDisplay();
-  display.setTextSize(text_size);
-
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // Display static text
   display.println(disp);
-  display.display(); 
-
+  display.display();
 }
 
-//Visual countdown on oled for when game is about to begin
-void oled_countdown(int time) 
-{
-  time = time * 1000;  // Convert time to milliseconds
-  int starttime = millis();
-  int endtime = starttime;
-  int lastUpdateTime = starttime;  // To track when to update the display
+void oled_display_score(byte score) {
+  display.clearDisplay();
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println(score);
+  display.display();
+}
 
-  while ((endtime - starttime) < time) {  // Run until the time limit is reached
-    endtime = millis();  // Update the current time
-
-    if (endtime - lastUpdateTime >= 1000) {  // If 1 second has passed
-      lastUpdateTime = endtime;  // Update the last update time
-      int secondsPassed = (endtime - starttime) / 1000;  // Calculate the seconds passed
-      oled_display_score(secondsPassed);  // Update the display
-    }
+void oled_countdown(int seconds) {
+  for (int i = seconds; i > 0; i--) {
+    oled_display_score(i);
+    waitMilliseconds(1000);
   }
 }
 
-// MOTOR FUNCTIONS 
+void waitMilliseconds(uint16_t ms) {
+  uint32_t start = millis();
+  while ((millis() - start) < ms) {
+    mp3.loop();
+    if(abs(analogRead(A3)-volume)>3)
+    {
+      volume = analogRead(A3)/20.5;
+      mp3.setVolume(volume);
+    }
+    delay(1);
+  }
+}
 
-//Rotates motor needs specific time delay step and angle 
 void rotate_motor(int t_step, int steps)
 {
   //Pins where motor is set up at 
@@ -474,7 +255,6 @@ void rotate_motor(int t_step, int steps)
   }
 }
 
-//Resets motor back to starting position (far right)
 void motor_reset()
  {
    int st_a = 5;
@@ -526,20 +306,3 @@ void motor_reset()
      delay(t_step);
    }
  }
-
-//MP3 Function
-
- void waitMilliseconds(uint16_t msWait)
-{
-    uint32_t start = millis();
-
-    while ((millis() - start) < msWait)
-    {
-        // if you have loops with delays, its important to 
-        // call dfmp3.loop() periodically so it allows for notifications 
-        // to be handled without interrupts
-        mp3.loop();
-        delay(1);
-    }
-}
- 
